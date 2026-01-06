cf30ebec07a180b85fbfc1c9ec5d250ecc6feb43
ebad-coder <ebad.adam2610@gmail.com>
2026-01-06 15:27:05 +0500

    solved day 6

--- code/day_6.c
+++ code/day_6.c
@@ -6,31 +6,47 @@ Ignore casing and white space.
 */
 
 #include <stdio.h>
-#include "../lib/utils.h"
+#include <string.h>
+#include <ctype.h>
 
-int is_anagram(char* str1, char* str2) {
-    remove_whitespaces(str1);
-    convert_to_lowercase(str1);
+int is_anagram(const char* str1, const char* str2)
+{
+    // array to keep track of frequency of each alphabet.
+    int count[26] = {0};
+    // store length of input strings
+    int n1 = strlen(str1);
+    int n2 = strlen(str2);
 
-    remove_whitespaces(str2);
-    convert_to_lowercase(str2);
-
-    int sum1 = 0, sum2 = 0;
-
-    for (int i = 0; str1[i] != '\0'; ++i) {
-        sum1 += str1[i];
+    // loop through the first input string
+    for (int i = 0; i < n1; ++i) {
+        // if the current character is an alphabet
+        if (isalpha(str1[i])) {
+            // then increment the characters index in the array
+            count[tolower(str1[i]) - 'a']++;
+        }
     }
 
-    for (int i = 0; str2[i] != '\0'; ++i) {
-        sum2 += str2[i];
+    // loop through the second input string
+    for (int i = 0; i < n2; ++i) {
+        // if the current character is an alphabet
+        if (isalpha(str2[i])) {
+            // then decrement the characters index in the array
+            count[tolower(str2[i]) - 'a']--;
+        }
     }
 
-    return sum1 == sum2;
+    // if both strings are anagrams, then each element of array should be 0.
+    for (int i = 0; i < 26; ++i) {
+        if (count[i] != 0) return 0;
+    }
+
+    // return true if the array test pass, meaning both strings are anagrams.
+    return 1;
 }
 
 int main()
 {
-    char str1[] = "listen";
-    char str2[] = "silent";
+    char str1[] = "Hello world";
+    char str2[] = "worLD, hello";
     printf("%d\n", is_anagram(str1, str2));
 }
